9e14e8c3b714f1a4e73bd4ceaa5b6b5b78aed3a3
Buck Melton <buckm@alumni.stanford.edu>
2025-02-28 10:00:23 -0800

    Add yearly investment results table

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,41 @@
-import { calculateInvestmentResults } from '../util/investment';
+import { calculateInvestmentResults, formatter } from '../util/investment';
 
 export default function Results({ userInput }) {
   const resultsData = calculateInvestmentResults(userInput);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].annualInvestment -
+    resultsData[0].interest;
+
   console.log(resultsData);
-  return (<p>Results...</p>);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Investment (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+
+          const totalInterest = yearData.valueEndOfYear - initialInvestment - (yearData.annualInvestment * yearData.year);
+          const investedCapital = initialInvestment + (yearData.annualInvestment * yearData.year);
+
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(investedCapital)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
