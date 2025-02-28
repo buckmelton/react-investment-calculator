d2a06b3959708d28e7dd89413ac32e518119864a
Buck Melton <buckm@alumni.stanford.edu>
2025-02-27 23:15:12 -0800

    Display current values in input fields

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -25,6 +25,7 @@ export default function UserInput() {
           <input
             type="number"
             required
+            value={userInput.initialInvestment}
             onChange={(event) => handleChange('initialInvestment', event.target.value)}
           />
         </p>
@@ -33,6 +34,7 @@ export default function UserInput() {
           <input
             type="number"
             required
+            value={userInput.annualInvestment}
             onChange={(event) => handleChange('annualInvestment', event.target.value)}
           />
         </p>
@@ -43,6 +45,7 @@ export default function UserInput() {
           <input
             type="number"
             required
+            value={userInput.expectedReturn}
             onChange={(event) => handleChange('expectedReturn', event.target.value)}
           />
         </p>
@@ -51,6 +54,7 @@ export default function UserInput() {
           <input
             type="number"
             required
+            value={userInput.duration}
             onChange={(event) => handleChange('duration', event.target.value)}
           />
         </p>
